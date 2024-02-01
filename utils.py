18f0767869b63fdaf9394608071512eef09cb121
Afonne-CID <delightinbusiness@gmail.com>
2024-02-01 16:48:26 +0100

    Update: Implement error handling

--- utils.py
+++ utils.py
@@ -17,8 +17,16 @@ def load_data(data_path):
     dict or None
         A Python dictionary representing the JSON data, or `None` if an error occurs.
     '''
-    with open(data_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+    try:
+        with open(data_path, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Error: File not found at {data_path}")
+    except json.JSONDecodeError:
+        print(f"Error: Unable to decode JSON in {data_path}")
+    except Exception as e: #pylint: disable=broad-exception-caught
+        print(f"An unexpected error occurred: {str(e)}")
+    return None
 
 def save_data(data_path, update_data):
     '''
