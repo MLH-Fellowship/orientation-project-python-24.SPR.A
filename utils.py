9a69a243f05a7bc73ec2a9edc3c45dbfa54e369b
Afonne-CID <delightinbusiness@gmail.com>
2024-02-01 13:31:53 +0100

    Add util function for saving data to source

--- utils.py
+++ utils.py
@@ -19,3 +19,21 @@ def load_data(data_path):
     '''
     with open(data_path, 'r', encoding='utf-8') as file:
         return json.load(file)
+
+def save_data(data_path, update_data):
+    '''
+    Save JSON data to a file.
+
+    Parameters
+    ----------
+    data_path : str
+        The path to the JSON file.
+    update_data : dict
+        The data to be saved as JSON.
+
+    Returns
+    -------
+    None
+    '''
+    with open(data_path, 'w', encoding='utf-8') as file:
+        json.dump(update_data, file)
