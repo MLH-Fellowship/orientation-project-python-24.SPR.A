f49c01950d3cb5f894ce00b03dd0868ec2a64ae8
Afonne-CID <delightinbusiness@gmail.com>
2024-02-01 13:31:02 +0100

    Add util function for loading data from source

--- utils.py
+++ utils.py
@@ -1,2 +1,21 @@
 '''A set of utility functions
 '''
+import json
+
+
+def load_data(data_path):
+    '''
+    Load JSON data from a file.
+
+    Parameters
+    ----------
+    data_path : str
+        The path to the JSON file.
+
+    Returns
+    -------
+    dict
+        A Python dictionary representing the JSON data.
+    '''
+    with open(data_path, 'r', encoding='utf-8') as file:
+        return json.load(file)
